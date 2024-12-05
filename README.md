0936986b68fd7f32e359698bfeb100be40d8a3fc
Rafael Guerra <rafael_63_@hotmail.com>
2024-12-05 19:22:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CURSO-HTML-CSS
+ Estudando HTML CSS CursoemVideo
---
+++ exercicios/ex008/TreinandoHoje/src/App.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       // System.out.println("Hello, World!");
+
+      /*  float janeiro = 7000;
+       float fevereiro = 7000;
+       float marco = 7000;
+
+       float soma = janeiro + fevereiro + marco;
+
+       float media = soma / 3;
+
+       System.out.println(soma);
+       System.out.println(media);
+       */
+
+       
+
+      /* if (valor % 2 == 0) {
+        System.out.println("PAR");
+        
+       } else {
+        System.out.println("IMPAR");
+       }
+        */
+       /*  System.out.println("Digite outro valor:");
+        int valor1 = tc.nextInt();
+
+        System.out.println("Digite outro valor:");
+        int valor2 = tc.nextInt();
+
+        int maior =0;
+
+       if (valor > maior) {
+            maior = valor;
+       }
+       if (valor1 > maior) {
+            maior = valor1;
+       }
+       if (valor2 > maior) {
+            maior = valor2;
+       }
+
+        System.out.println(maior);
+        */
+       /*  int i = 0;
+        while (i <= 10) {
+            int mult = valor * i;
+            System.out.println(valor +  " + " + i +  " = " + mult);
+            i++;
+        }
+        */
+
+       
+       // Fecha o objeto Scanner
+       //scanner.close();
+
+        Scanner tc = new Scanner(System.in);
+       int[] barco =  new int[10];
+
+       System.out.println("Digite o nome dos barcos?");
+        
+
+
+
+
+
+
+       
+
+
+
+    }
+}
